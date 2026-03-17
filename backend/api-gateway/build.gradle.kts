18db35a9fceb5dfccb1ca16ac9bc60f18927bdf7
beomsic <hhqjatjr@gmail.com>
2026-03-18 00:47:35 +0900

    feat: ApiGateway 모듈 설정

---
+++ backend/api-gateway/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.cloud:spring-cloud-gateway-server-webflux:5.0.1")
+    implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer:5.0.1")
+}
