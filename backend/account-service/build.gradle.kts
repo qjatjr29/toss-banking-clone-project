12967ff7fddcfc3a367599abc83d51dbbe0c38a9
beomsic <hhqjatjr@gmail.com>
2026-03-18 10:30:44 +0900

    chore: WebFlux 의존성 추가 및 테스트 환경 구성
    - spring-boot-starter-webflux 의존성 추가
    - Kotest 기반 테스트 환경 설정
    - MockK 의존성 추가
    - coroutine 테스트 지원 추가

--- backend/account-service/build.gradle.kts
+++ backend/account-service/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":common"))
-//    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
 
     // MySQL & JPA
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -13,4 +13,14 @@ dependencies {
     // Redis (Caching & Distributed Lock)
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation("org.redisson:redisson-spring-boot-starter:4.3.0")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+	testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+	// Kotest 검증(Assertion) 라이브러리 (shouldBe 등)
+	testImplementation("io.kotest:kotest-assertions-core:5.8.0")
+	// Kotest에서 @SpringBootTest 등 스프링 빈을 주입받기 위한 확장 모듈
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.mockk:mockk:1.13.10")
 }
