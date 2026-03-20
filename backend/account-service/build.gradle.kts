9d10886fdf96990e09dd969df542590c7bb28f31
beomsic <hhqjatjr@gmail.com>
2026-03-20 15:06:54 +0900

    remove: 불필요한 주석 삭제

--- backend/account-service/build.gradle.kts
+++ backend/account-service/build.gradle.kts
@@ -30,6 +30,5 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:testcontainers:2.0.3")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
-//    testImplementation("org.testcontainers:junit-jupiter:1.21.4")
     testImplementation("org.testcontainers:mysql:1.21.4")
 }
