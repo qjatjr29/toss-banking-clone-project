4df65ecce4e5dca4f57e9b018658e8cd5a99c87b
beomsic <hhqjatjr@gmail.com>
2026-03-26 12:44:48 +0900

    feat: 스케줄러 활성화

--- backend/account-service/src/main/kotlin/com/tossbank/account/AccountServiceApplication.kt
+++ backend/account-service/src/main/kotlin/com/tossbank/account/AccountServiceApplication.kt
@@ -2,8 +2,10 @@ package com.tossbank.account
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class AccountServiceApplication
 
 fun main(args: Array<String>) {
