65e64932a625adea92664340ced7cc15c4ae36e8
beomsic <hhqjatjr@gmail.com>
2026-05-20 20:26:25 +0900

    feat: OutboxEventType, Topics에 타행 이체 재조회 이벤트 추가
    - OutboxEventType에 INTERBANK_TRANSFER_INQUIRY 추가
    - Topics에 transfer.interbank.inquiry 토픽 추가

--- backend/transfer-service/src/main/kotlin/com/tossbank/transfer/infrastructure/outbox/OutboxEventType.kt
+++ backend/transfer-service/src/main/kotlin/com/tossbank/transfer/infrastructure/outbox/OutboxEventType.kt
@@ -5,11 +5,13 @@ import com.tossbank.transfer.infrastructure.kafka.Topics
 enum class OutboxEventType {
     WITHDRAW_INQUIRY,    // 출금 여부 재조회
     DEPOSIT_INQUIRY,     // 입금 여부 재조회
-    COMPENSATE_WITHDRAW; // 출금 취소 보장 트랜잭션
+    COMPENSATE_WITHDRAW, // 출금 취소 보장 트랜잭션
+    INTERBANK_TRANSFER_INQUIRY; // 외부 송금 결과 재조회
 
     fun toKafkaTopic() = when (this) {
         WITHDRAW_INQUIRY    -> Topics.WITHDRAW_INQUIRY
         DEPOSIT_INQUIRY     -> Topics.DEPOSIT_INQUIRY
         COMPENSATE_WITHDRAW -> Topics.WITHDRAW_CANCEL
+        INTERBANK_TRANSFER_INQUIRY  -> Topics.INTERBANK_TRANSFER_INQUIRY
     }
 }
